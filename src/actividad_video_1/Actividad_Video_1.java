8e69542e940398d0c3d95cbfe0a8a049f60911f9
FiorellaAilen <fiorellaailen@gmail.com>
2023-08-18 19:48:27 -0300

    Initial commit

---
+++ src/actividad_video_1/Actividad_Video_1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad_video_1;
+
+/**
+ *
+ * @author Fiorella Ailén
+ */
+public class Actividad_Video_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
